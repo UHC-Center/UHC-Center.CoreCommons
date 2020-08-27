1ac31c81219c51b47bacbbf85e05da3ef8df96ba
Gabriella <isgabriellacurious@gmail.com>
2020-08-27 21:17:42 +0100

    quicc update

--- src/main/java/center/uhc/core/commons/FileUtil.java
+++ src/main/java/center/uhc/core/commons/FileUtil.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class FileUtil {
 
@@ -60,4 +61,7 @@ public class FileUtil {
         FileUtils.copyDirectory(i, f);
     }
 
+    public static void copyFile(File initial, File location) throws IOException {
+        FileUtils.copyFileToDirectory(initial, location);
+    }
 }
